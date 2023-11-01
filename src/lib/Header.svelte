5ebcbb0ced6f784fe7c3d06d7932bb13259eb1c6
Dovi Cowan <dovi@fullynetworking.co.uk>
2023-11-01 23:41:48 +0000

    linting

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,10 +1,23 @@
 <script>
-    import { logOut, loggedInUser } from "./appwrite";
-
-
+  import { account, logOut, loggedInUser } from "./appwrite";
 </script>
 
-<div class="">Welcome, {loggedInUser.name} <a href="#" class="text-blue-400" on:click={logOut}>Log out</a></div>
+<div class="">
+  Welcome, {loggedInUser.name}
+  <a href="#" class="text-blue-400" on:click={logOut}>Log out</a>
+</div>
 {#if !loggedInUser.emailVerification}
-  <div class="">You have not verified your email address yet</div>
+  <div class="">
+    You have not verified your email address yet <a
+      href="#"
+      class="text-blue-400"
+      on:click={() =>
+        account.createVerification(
+          window.location.protocol +
+            "//" +
+            window.location.host +
+            "/register/emailVerify",
+        )}>Resend verification email</a
+    >
+  </div>
 {/if}
